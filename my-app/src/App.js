66219acc18c04987418769ea621bfa50de8850eb
グエン ティエンマイン <dev10157@blcloudprj101.sakura.ne.jp>
2020-09-11 09:12:34 +0700

    form-start

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    name: 'Ryu',
+    age: 30
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>My name is {this.state.name}</h1>
+        <form>
+          <input type="text"/>
+        </form>
+      </div>
+    );
+  }
+
 }
 
 export default App;
