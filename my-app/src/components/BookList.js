1b853291cd5263e0c4a70568664916dac5f04875
buiminh15 <40208756+buiminh15@users.noreply.github.com>
2020-09-19 07:48:58 +0700

    context

---
+++ my-app/src/components/BookList.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class BookList extends Component {
+    render() {
+        return (
+          <div className="book-list">
+            <ul>
+              <li>the way of king</li>
+              <li>the tottochan next to window</li>
+              <li>the final empire</li>
+            </ul>
+          </div>
+        );
+    }
+}
